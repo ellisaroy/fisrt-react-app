d40c9c909ca78fad8db59fcd0e128132335a7425
ellisaroy <anniesaroy10@gmail.com>
2022-09-06 17:50:51 +0530

    map

--- src/pages/Methods.js
+++ src/pages/Methods.js
@@ -3,10 +3,13 @@ import React from 'react';
 const Methods = () =>{
     const numbers = [1,7,2,3,8];
         const newNumbers = numbers.reduce((accumulator,currentValue)=>accumulator+currentValue);
+    const cal = [3,4,5,6,7,8,9]
+        const newCal = cal.filter((cal)=>cal >= 6);
     return(
         <><h1>Example of array methods</h1>
             <h2>Reduce Method</h2>
             <p>Here is the new value ge by reduce method:{newNumbers}</p>
+            <p>Map methods used {newCal}</p>
         </>
     );
 };
