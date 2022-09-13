e950adc164d09facdebdc894f8bc34babf4e8dfe
ellisaroy <anniesaroy10@gmail.com>
2022-09-13 13:49:55 +0530

    fsdf

--- src/pages/Methods.js
+++ src/pages/Methods.js
@@ -15,7 +15,9 @@ const Methods = () =>{
     const concatBrands2 = ["Nexa","Ford"];
     const concatBrands3 = ["Cecilie","Linus"];
     const newArray = concatBrands1.concat(concatBrands2,concatBrands3);
-    //const ages = [12,34,56,78];
+    const ages = [12,34,56,78];
+        const checkAge = ages.every((ages) => ages > 18 );
+        console.log(checkAge);
     const colors = ["Red","Black","Blue","Icy Blue","Green"];
     const colorResult = colors.fill("White",2,4);
     return(
@@ -25,9 +27,7 @@ const Methods = () =>{
             <p>Map methods used {newCal}</p>
             <p>Numbers greater than 21 are:{findNumber}</p>
             <p>Conact arrays: {newArray}</p>
-            {/* <p>Every method output: {result}</p> */}
             <p>Fill result: {colorResult}</p>
-
         </>
     );
 };
