22cd6d84c84671a9a1457348ffc0d48c630770a4
ellisaroy <anniesaroy10@gmail.com>
2022-09-13 13:37:36 +0530

    array

--- src/pages/Methods.js
+++ src/pages/Methods.js
@@ -11,13 +11,23 @@ const Methods = () =>{
     const findNum = [32,56,21,32,45];
         const findNumber = findNum.find((number)=> number >=  21);
         console.log(findNumber);
+    const concatBrands1 = ["Audi","Tata","Hyundai"];
+    const concatBrands2 = ["Nexa","Ford"];
+    const concatBrands3 = ["Cecilie","Linus"];
+    const newArray = concatBrands1.concat(concatBrands2,concatBrands3);
+    //const ages = [12,34,56,78];
+    const colors = ["Red","Black","Blue","Icy Blue","Green"];
+    const colorResult = colors.fill("White",2,4);
     return(
         <>
             <h1>Example of array methods</h1>
-            <h2>Reduce Method</h2>
             <p>Here is the new value ge by reduce method:{newNumbers}</p>
             <p>Map methods used {newCal}</p>
             <p>Numbers greater than 21 are:{findNumber}</p>
+            <p>Conact arrays: {newArray}</p>
+            {/* <p>Every method output: {result}</p> */}
+            <p>Fill result: {colorResult}</p>
+
         </>
     );
 };
