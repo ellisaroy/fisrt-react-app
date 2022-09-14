2aa29894e30754496593ab1b3d3259618fb08421
ellisaroy <anniesaroy10@gmail.com>
2022-09-14 17:22:11 +0530

    post

--- src/components/Postdata..js
+++ src/components/Postdata..js
@@ -2,18 +2,34 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 const PostData = () => {
   const baseURL = "https://jsonplaceholder.typicode.com/posts";
-  const [post, setPost] = useState(null);
+  const [posts, setPost] = useState();
   useEffect(() => {
     axios.get(baseURL).then((response) => {
-      setPost(response.data);
-      console.log(response.data);
+      //setPost(response.data);
     });
   }, []);
+  function createPost() {
+    axios
+      .post(baseURL, {
+        title: "Hello World!",
+        body: "This is a new post."
+      })
+      .then((response) => {
+        setPost(response.data);
+      });
+  }
 
   return (
     <>
       <div className="postdata-wrapper">
-        <p></p>
+        <ul>
+          {posts &&
+            posts.length > 0 &&
+            posts.map((post) => <li key={post.id}>{post.title} </li>)}
+        </ul>
+        {/* <h1>{post.title}</h1>
+        <p>{post.body}</p> */}
+      <button onClick={createPost}>Create Post</button>
       </div>
     </>
   );
